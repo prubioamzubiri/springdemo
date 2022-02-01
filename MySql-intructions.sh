3834fb578fda23e213ff4a9fc446fef296714045
prubioamzubiri <prubioam@zubirimanteo.com>
2022-02-01 12:04:48 +0000

    Cambiada la documentación para levantar mysql docker

--- MySql-intructions.sh
+++ MySql-intructions.sh
@@ -2,6 +2,8 @@
 # Pull latest dockerHub image from mysql
 # docker pull mysql
 # Runs mysql docker with 'Tknika1' as root password and 'my-sql' as server name
-docker run --name mysql-server -e MYSQL_ROOT_PASSWORD=Tknika1 -d mysql
+# docker run --name mysql-server -e MYSQL_ROOT_PASSWORD=Tknika1 -d mysql
 # Conect to mysql server with root user from command line
-docker exec -it mysql-server mysql -uroot -p
+# docker exec -it mysql-server mysql -uroot -p
+
+ docker run --detach --name=prueba -p 3306:3306 --env="MYSQL_ROOT_PASSWORD=1234" mysql
